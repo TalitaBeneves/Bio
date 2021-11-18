c5a64a394c468f0cd33a523bbd3fea1075b528c1
Talita Beneves <talitabeneves15@gmail.com>
2021-11-17 23:36:47 -0300

    updates

--- src/components/Group.js
+++ src/components/Group.js
@@ -10,25 +10,26 @@ const Group = () => {
     <div className="container">
       <img src={foto} alt="foto" />
 
-      <h3 className="text gradient__text">
-        Talita Beneves <br/> 19 anos <br/> Rio de Janeiro
+      <h3 className="text">
+        Talita Beneves <br/> 19 anos <br/>Rio de Janeiro
       </h3>
 
       <div className="social" id="linkedin">
-      <h2><a href="https://www.linkedin.com/in/talitabeneves"><FaLinkedinIn id="icon"/> Linkedin </a> </h2> 
-      </div>
-        
-      <div className="social" id="portfolio">
-        <h2><a href=""><MdComputer id="icon"/> Portfólio </a> </h2> 
-      </div>
-
-      <div className="social" id="instagram">
-        <h2><a href="https://www.instagram.com/talita_beneves/"><FaInstagram id="icon"/> Instagram </a> </h2> 
+      <h2><a href="https://www.linkedin.com/in/talitabeneves" rel="noreferre" target="_blank" ><FaLinkedinIn id="icon"/> Linkedin </a> </h2> 
       </div>
 
       <div className="social" id="github" >
-        <h2><a href="https://github.com/TalitaBeneves"><FaGithub id="icon"/> Github </a> </h2> 
+        <h2><a href="https://github.com/TalitaBeneves" rel="noreferre" target="_blank" ><FaGithub id="icon"/> Github </a> </h2> 
       </div>
+        
+      <div className="social" id="portfolio">
+        <h2><a href="" rel="noreferre" target="_blank" ><MdComputer id="icon"/> Portfólio </a> </h2> 
+      </div>
+
+      <div className="social" id="instagram">
+        <h2><a href="https://www.instagram.com/talita_beneves/" rel="noreferre" target="_blank" ><FaInstagram id="icon"/> Instagram </a> </h2> 
+      </div>
+
     </div>
   );
 }
